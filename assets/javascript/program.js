6714a78feb655eea08b588aa625e519d2286e319
Armando Morales <Armando Morales>
2016-09-10 01:37:36 -0500

    Created visual layout of page.

---
+++ assets/javascript/program.js
@@ -0,0 +1,8 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBJlXMv2SHC0KPrmjGG3bqMlf3digTt9t4",
+    authDomain: "hw7-train-scheduling.firebaseapp.com",
+    databaseURL: "https://hw7-train-scheduling.firebaseio.com",
+    storageBucket: "",
+  };
+  firebase.initializeApp(config); 
